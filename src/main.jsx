b1437ba1545d6b0b4de198ecc18fcefb3cb3d62d
IsabelleNunes <bellefsnunes@gmail.com>
2024-05-07 01:34:56 -0300

    feat(error-page): insert error page component
    Component used by error in routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,18 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Root from './routes/root';
+import ErrorPage from "./error-page";
 
 const router = createBrowserRouter([{
   path: "/", 
-  element: <div> Hello World! </div>
+  element: <Root />,
+  errorElement: <ErrorPage />
 }
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
